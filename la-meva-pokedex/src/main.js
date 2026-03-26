29dd2dfe7fc3b5ba8594e262c12923a8c9b0d253
Salva <ssantamaria@institutmvm.cat>
2026-03-26 10:02:07 +0100

    busca pokemons

--- la-meva-pokedex/src/main.js
+++ la-meva-pokedex/src/main.js
@@ -4,9 +4,21 @@ import { createCard, GetInput }  from "./ui.js"
 
 const Buscar = document.getElementById("BotoBuscar");
 const missatges = document.getElementById("missatges");
+const PokemonBuscado = document.getElementById("pokemonEncontrado");
+const AddFav = document.getElementsByClassName("btn-fav");
 Buscar.addEventListener("click", async () => {
-    const NomPokemon = await GetInput()
-    missatges.innerHTML = `Cercant a ${NomPokemon}...`
-    const DadesPokemon = await getPokemon(NomPokemon)
-    createCard(DadesPokemon)
+    try {
+        PokemonBuscado.innerHTML = ""
+        const NomPokemon = GetInput()
+        missatges.innerHTML = `Cercant a ${NomPokemon}...`
+        const DadesPokemon = await getPokemon(NomPokemon);
+        const CartaPokemon = createCard(DadesPokemon);
+        missatges.innerHTML = ""
+        PokemonBuscado.appendChild(CartaPokemon)
+    } catch (error) {
+        missatges.innerHTML = "Pokemon no trobat"
+    }
+});
+AddFav.addEventListener("click", async () => {
+    MoveCard()
 });
