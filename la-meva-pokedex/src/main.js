10f33c59fcf49e1d926cabc2aa320a0ab72f048a
Salva <ssantamaria@institutmvm.cat>
2026-03-26 09:34:59 +0100

    avance main.js y arreglos en html

--- la-meva-pokedex/src/main.js
+++ la-meva-pokedex/src/main.js
@@ -1,3 +1,12 @@
 import { getPokemon } from "./api.js"
 import * as db from "./storage.js"
-import { createCard}  from "./ui.js"
+import { createCard }  from "./ui.js"
+
+const Buscar = document.getElementById("BotoBuscar");
+const missatges = document.getElementById("missatges");
+Buscar.addEventListener("click", async () => {
+    missatges.innerHTML = `Cercant dades...`
+    const NomPokemon = GetInput()
+    const DadesPokemon = await getPokemon(NomPokemon)
+    createCard(DadesPokemon)
+});
